c494cf005a7543b5c11ccaabf0b72328d119f694
satyam prajapati065 <satyamprajapati065@gmail.com>
2026-05-30 10:05:08 +0530

    add user name

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const TelegramBot = require("node-telegram-bot-api");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const axios = require("axios");
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
-// bot.on("message", (option) => {
-//   console.log("message recieved on the bot", option);
-//   bot.sendMessage(
-//     option.chat.id,
-//     "Hello, I am a bot. I am here to help you with your queries. Please type /help to know more about me.",
-//   );
-// });
+bot.onText(/\/start/, (option) => {
+  bot.sendMessage(
+    option.chat.id,
+    `Hello ${option.from.first_name}, I am a bot. I am here to help you with your queries. Please type /joke to get a funny joke!`,
+  );
+  console.log(option);
+});
 
 bot.onText(/\/joke/, async (option) => {
-  const response = await axios.get(
-    "https://official-joke-api.appspot.com/random_joke",
-  );
-  const setup = response.data.setup;
-  const punchline = response.data.punchline;
-  bot.sendMessage(option.chat.id, setup + " ,  " + punchline);
+  try {
+    const response = await axios.get(
+      "https://official-joke-api.appspot.com/random_joke",
+    );
+    console.log(response);
+    const setup = response.data.setup;
+    const punchline = response.data.punchline;
+    bot.sendMessage(option.chat.id, `${setup}\n\n😂 ${punchline}`);
+  } catch (error) {
+    console.error("Joke fetch karne me error aaya:", error.message);
+    bot.sendMessage(
+      option.chat.id,
+      "Sorry bhai, abhi mood nahi hai joke sunane ka. (API Error)",
+    );
+  }
 });
